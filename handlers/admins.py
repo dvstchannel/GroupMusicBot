72799f85e5f23f08d931abb42c84123ce8596c51
dvstchannel <83810390+dvstchannel@users.noreply.github.com>
2021-06-10 08:24:33 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -9,7 +9,7 @@ from helpers.filters import command, other_filters
 from helpers.decorators import errors, authorized_users_only
 
 
-@Client.on_message(command("pause") & other_filters)
+@Client.on_message(command("tamdung") & other_filters)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
@@ -18,13 +18,13 @@ async def pause(_, message: Message):
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'paused'
     ):
-        await message.reply_text("❗ Nothing is playing!")
+        await message.reply_text("❗ Không có gì đang chơi!")
     else:
         callsmusic.pytgcalls.pause_stream(message.chat.id)
-        await message.reply_text("▶️ Paused!")
+        await message.reply_text("▶️ Bị tạm dừng!")
 
 
-@Client.on_message(command("resume") & other_filters)
+@Client.on_message(command("tieptuc") & other_filters)
 @errors
 @authorized_users_only
 async def resume(_, message: Message):
@@ -33,18 +33,18 @@ async def resume(_, message: Message):
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'playing'
     ):
-        await message.reply_text("❗ Nothing is paused!")
+        await message.reply_text("❗ Không có gì bị tạm dừng!")
     else:
         callsmusic.pytgcalls.resume_stream(message.chat.id)
-        await message.reply_text("⏸ Resumed!")
+        await message.reply_text("⏸ Tiếp tục!")
 
 
-@Client.on_message(command("end") & other_filters)
+@Client.on_message(command("tatnhac") & other_filters)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing is streaming!")
+        await message.reply_text("❗ Không có gì đang phát trực tuyến!")
     else:
         try:
             callsmusic.queues.clear(message.chat.id)
@@ -52,15 +52,15 @@ async def stop(_, message: Message):
             pass
 
         callsmusic.pytgcalls.leave_group_call(message.chat.id)
-        await message.reply_text("❌ Stopped streaming!")
+        await message.reply_text("❌ Đã dừng phát trực tuyến!")
 
 
-@Client.on_message(command("skip") & other_filters)
+@Client.on_message(command("boqua") & other_filters)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ Nothing is playing to skip!")
+        await message.reply_text("❗ Không có gì đang chơi để bỏ qua!")
     else:
         callsmusic.queues.task_done(message.chat.id)
 
@@ -72,4 +72,4 @@ async def skip(_, message: Message):
                 callsmusic.queues.get(message.chat.id)["file"]
             )
 
-        await message.reply_text("➡️ Skipped the current song!")
+        await message.reply_text("➡️ Đã bỏ qua bài hát hiện tại!")
